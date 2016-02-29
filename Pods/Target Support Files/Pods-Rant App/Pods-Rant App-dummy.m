c4d93522bbafd32f5cd9f66c647f4123c24b9193
Aaron Epstein <a.epstein7ae@gmail.com>
2016-02-29 14:32:56 -0500

    Storyboard edits

---
+++ Pods/Target Support Files/Pods-Rant App/Pods-Rant App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rant_App : NSObject
+@end
+@implementation PodsDummy_Pods_Rant_App
+@end
---
+++ Pods/Target Support Files/Pods-Rant App/Pods-Rant App-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Rant_AppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Rant_AppVersionString[];
+
